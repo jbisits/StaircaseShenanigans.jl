3280fc4c18f8ac8d6ba13076d324fd3c51d4e7c5
jbisits <jbisits@gmail.com>
2024-07-30 20:25:22 +1000

    Update to R_rho + correctly set coefficients in linear eos

--- examples/four_stair_staircase.jl
+++ examples/four_stair_staircase.jl
@@ -11,7 +11,7 @@ model = DNSModel(architecture, domain_extent, resolution, diffusivities)
 ## Initial conditions
 number_of_steps = 4
 depth_of_steps = [-0.2, -0.4, -0.6, -0.8]
-salinity = [34.57, 34.6, 34.63, 34.66, 34.69]
+salinity = [34.57, 34.60, 34.63, 34.66, 34.69]
 temperature = [-1.5, -1.0, -0.5, 0.0, 0.5]
 
 step_ics = StepInitialConditions(model, number_of_steps, depth_of_steps, salinity, temperature)
@@ -21,7 +21,7 @@ sdns = StaircaseDNS(model, step_ics)
 set_staircase_initial_conditions!(sdns)
 
 Δt = 1e-2
-stop_time = 2* 60 # seconds
+stop_time = 2 * 60 # seconds
 save_schedule = 60  # seconds
 output_path = joinpath(@__DIR__, "output")
 simulation = SDNS_simulation_setup(sdns, Δt, stop_time, save_schedule, save_computed_output!; output_path)

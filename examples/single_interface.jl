fd2c2e12fcb5e6b2c8877fce8f2ad71266b4f5a1
jbisits <jbisits@gmail.com>
2024-10-10 12:29:06 +1100

    Add restoring for single interface

---
+++ examples/single_interface.jl
@@ -0,0 +1,41 @@
+using StaircaseShenanigans
+
+architecture = CPU() # or GPU()
+diffusivities = (ν = 1e-4, κ = (S = 1e-7, T = 1e-5))
+domain_extent = (Lx = 0.1, Ly = 0.1, Lz = -1.0)
+resolution = (Nx = 5, Ny = 5, Nz = 50)
+
+## Initial conditions
+number_of_steps = 1
+depth_of_steps = [-0.5]
+salinity = [34.58, 34.70]
+temperature = [-1.5, 0.5]
+
+## Setup the model
+eos = CustomLinearEquationOfState(0, 34.6)
+rate = 1/10
+mask = OuterMask(-0.25, -0.75)
+S_target = OuterTargets(34.58, 34.7, mask)
+T_target = OuterTargets(-1.5, 0.5, mask)
+T_restoring = Relaxation(; rate, mask, target = T_target)
+S_restoring = Relaxation(; rate, mask, target = S_target)
+relaxation = (S = S_restoring, T = T_restoring)
+model = DNSModel(architecture, domain_extent, resolution, diffusivities; relaxation)
+
+## Set initial conditions
+step_ics = StepInitialConditions(model, number_of_steps, depth_of_steps, salinity, temperature)
+
+sdns = StaircaseDNS(model, step_ics)
+
+set_staircase_initial_conditions!(sdns)
+
+## Build simulation
+Δt = 1e-1
+stop_time = 2 * 60 # seconds
+save_schedule = 10  # seconds
+output_path = joinpath(@__DIR__, "output")
+simulation = SDNS_simulation_setup(sdns, Δt, stop_time, save_schedule, save_computed_output!;
+                                   output_path, max_Δt = 5)
+
+## Run
+run!(simulation)

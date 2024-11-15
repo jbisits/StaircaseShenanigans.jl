2b0cbabc489adf8ca4ad75d1266abf231fc086b5
jbisits <jbisits@gmail.com>
2024-11-15 19:48:28 +1100

    export the functions

--- src/StaircaseShenanigans.jl
+++ src/StaircaseShenanigans.jl
@@ -43,7 +43,8 @@ export save_computed_output!, save_all_velocities!, save_vertical_velocities!
 
 export compute_R_ρ!
 
-export animate_tracers, animate_density, visualise_initial_conditions, visualise_initial_density
+export animate_tracers, animate_density, visualise_initial_conditions, visualise_initial_density,
+       animate_tracers_anomaly, animate_density_anomaly
 
 include("staircase_initial_conditions.jl")
 include("staircase_noise.jl")

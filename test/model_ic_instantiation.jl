a24b1c002e769ed786785d21fc444636ba6f51f4
jbisits <jbisits@gmail.com>
2024-12-05 14:48:11 +1100

    Add instantiation test

--- test/model_ic_instantiation.jl
+++ test/model_ic_instantiation.jl
@@ -3,5 +3,5 @@ depth_of_interface =  -0.6
 salinity = [34.57, 34.69]
 temperature = [-1.5, 0.5]
 
-smoothing = (NoSmoothing, Tanh)
-background = (NoBackground, BackgroundTanh, BackgroundLinear)
+smoothing = (NoSmoothing, Tanh())
+background = (NoBackground, BackgroundTanh(), BackgroundLinear(), BackgroundStep())

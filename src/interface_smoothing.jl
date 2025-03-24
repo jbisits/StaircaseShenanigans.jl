73c91c7d53935ca33105ad5daedbb92928c3b659
jbisits <jbisits@gmail.com>
2025-03-25 09:14:57 +1100

    Docstring update

--- src/interface_smoothing.jl
+++ src/interface_smoothing.jl
@@ -17,7 +17,7 @@ end
 const Tanh = TanhInterfaceSmoothing{T} where {T}
 @inline (p::TanhInterfaceSmoothing)(x, y, z) = p.Cₗ - 0.5 * p.ΔC * (1  + tanh((z - p.z_interface) / (p.h * p.z_range)))
 
-"Container for the steepness of the `tanh` over the interface for both salinity and temperature.
+"Container for the initial thickness of the `tanh` for both salinity and temperature.
 Allows easy setting of both and avoids having to set other parts of the container before the
 model has been setup."
 struct TanhInterfaceThickness{T}
